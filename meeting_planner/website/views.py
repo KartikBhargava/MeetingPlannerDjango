4364fa19d04364020fe2e563878500ff50c02a26
KartikBhargava <bhargava.kartik07@gmail.com>
2020-05-07 23:10:50 +0530

    Model class created and migrated

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,10 @@ from django.http import HttpResponse
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
 
+
+def date(request):
+    return HttpResponse("This page was Served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("Hello, My name is Kartik.")
