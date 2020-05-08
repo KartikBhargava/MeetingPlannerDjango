07b00d745ed7a8add3f6463422046fc89e3f9573
KartikBhargava <bhargava.kartik07@gmail.com>
2020-05-08 06:19:47 +0530

    Models Created. Url Linking done. html inheritence done. styling using css done.

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,7 +1,22 @@
+from datetime import time
 from django.db import models
 
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    floor_number = models.IntegerField(default=0)
+    room_number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.name} on {self.floor_number} in {self.room_number}"
+
+
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % self.title
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
 
 
 def date(request):
